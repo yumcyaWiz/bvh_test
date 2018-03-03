9d3cc4f3c3fd27bb215fe0bed42b32ebf032f88a
yumcyawiz <sergeyinverse@gmail.com>
2018-03-03 16:46:07 +0900

    add primitive.h

---
+++ primitive.h
@@ -0,0 +1,47 @@
+#ifndef PRIMITIVE_H
+#define PRIMITIVE_H
+#include "vec3.h"
+class Primitive {
+    public:
+        virtual bool intersect(const Ray& ray, const Hit& hit) const = 0;
+};
+
+
+class Sphere : Primitive {
+    public:
+        Vec3 center;
+        float radius;
+
+        Sphere() {};
+
+        bool intersect(const Ray& ray, const Hit& hit) const {
+            return false;
+        };
+};
+
+
+class Triangle : Primitive {
+    public:
+        Vec3 p1;
+        Vec3 p2;
+        Vec3 p3;
+
+        Triangle() {};
+
+        bool intersect(const Ray& ray, const Hit& hit) const {
+            return false;
+        };
+};
+
+
+class AABB : Primitive {
+    public:
+        Vec3 pMin;
+        Vec3 pMax;
+
+        AABB() {};
+
+        bool intersect(const Ray& ray, const Hit& hit) const {
+            return false;
+        };
+};
